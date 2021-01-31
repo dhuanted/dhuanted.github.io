610b1d21507eda76d470fa48f2f736526021aa74
dhuanted <dhuanted@gmail.com>
2021-01-30 20:18:50 -0500

    210131 2018

---
+++ lesson4/js/script.js
@@ -0,0 +1,33 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let fullDate =
+    dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+  document.getElementById("currentDate").textContent = fullDate;
+  document.getElementById("currentYear").textContent = d.getFullYear();
+  let vLastModified = new Date(document.lastModified);
+  document.getElementById("lastModified").textContent = vLastModified;
+  
