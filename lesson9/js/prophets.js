8a21722cf9333a9ceca9df90db3ffe718f0c5661
dhuanted <dhuanted@gmail.com>
2021-03-06 22:51:31 -0500

    20210306 2251

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,17 +10,22 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
-let p = document.createElement('p');
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
+let p3 = document.createElement('p');
 let image =document.createElement('img');
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + '. ' + ' ' + 
-'Place of Birth:' + ' ' + prophets[i].birthplace;
+p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+p3.textContent = 'Death: ' + prophets[i].death;
 image.setAttribute('src', prophets[i].imageurl);
 image.setAttribute( 'alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + prophets[i].order);
 
 card.appendChild(h2);
-card.appendChild(p);
+card.appendChild(p1);
+card.appendChild(p2);
+card.appendChild(p3);
 card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
